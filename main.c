f910c79b65949452554d0f4929ed4232bd24d5ce
bth123 <m1207pro@gmail.com>
2026-09-14 20:33:59 +0300

    logos

--- main.c
+++ main.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 #include <sys/utsname.h>
 
+const char logo_padding = 4;
+
+void read_logo(char* path, char* out) {
+	FILE* logo_file = fopen(path, "r");
+	char[72] buf;
+	
+	fclose(logo_file);
+}
+
+int count_termux_packages() {
+	FILE *status_list = fopen("/data/data/com.termux/files/usr/var/lib/dpkg/status", "r");
+	if (status_list == NULL) {
+		return -1;
+	}
+
+	int count = 0;
+	char buf[256];
+	if (fscanf(status_list, "Status: install ok installed", buf)) {
+		count++;
+	}
+}
+
+
 char is_android(void) {
 	return getenv("ANDROID_ROOT") != NULL;
 }
@@ -23,11 +46,11 @@ void get_sysname(char* os_name) {
             char buf[72];
             if (fscanf(distro_meta, "NAME=\"%s\"", buf) == 1) {
                 strcpy(os_name, buf);
-                fclose(distro_meta);
             }
         } else if (is_android()) {
             strcpy(os_name, "Android");
         }
+        fclose(distro_meta);
     }
 }
 
